0c33891158787ac327dd5764b25da333280c42b7
Rafaela <rafaela.azevedosouza@gmail.com>
2020-12-06 17:47:58 +0000

    Added new field

--- src/product/product.js
+++ src/product/product.js
@@ -1,9 +1,10 @@
 class Product {
-    constructor(id, type, name, version) {
+    constructor(id, type, name, version, brand) {
         this.id = id;
         this.type = type;
         this.name = name;
         this.version = version;
+        this.brand = brand;
     }
 }
 
